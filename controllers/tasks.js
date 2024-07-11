21e6282dd291d649f32aa1bdc126b2a46cd1ab61
ashutosh1506 <guptaashutosh502@gmail.com>
2024-07-11 12:27:41 +0530

    DB,middleware,routes,controllers Setup

---
+++ controllers/tasks.js
@@ -0,0 +1,18 @@
+const getAllTasks = (req, res) => {
+  res.send("All Items From the FIle");
+};
+
+const createTask = (req, res) => {
+  res.json(req.body);
+};
+const getTask = (req, res) => {
+  res.json({ id: req.params.id });
+};
+const updateTask = (req, res) => {
+  res.send("Update Task");
+};
+const deleteTask = (req, res) => {
+  res.send("Delete Task");
+};
+
+module.exports = { getAllTasks, createTask, getTask, updateTask, deleteTask };
---
+++ db/connect.js
@@ -0,0 +1,2 @@
+const connectionString =
+  "mongodb+srv://ashu:<password>@nodeexpressprojects.lulmkks.mongodb.net/?retryWrites=true&w=majority&appName=NodeExpressProjects";
