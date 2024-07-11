ec847f7e3c9b3fa527d3fba9df29806f3053680c
ashutosh1506 <guptaashutosh502@gmail.com>
2024-07-11 11:34:07 +0530

    created server

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log('Task Manager App')
+const express = require("express");
+const app = express();
+
+//routes
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+const port = 3000;
+app.listen(port, console.log("Server is listening on port 3000..."));
