fd7cb3cc388057aff88de014b00e1ce7ded21df2
ashutosh1506 <guptaashutosh502@gmail.com>
2024-07-11 12:56:55 +0530

    DB Setup

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
-
+const connectDB = require("./db/connect");
+require("dotenv").config();
 //middleware
 app.use(express.json());
 
@@ -9,8 +10,17 @@ app.use(express.json());
 app.get("/hello", (req, res) => {
   res.send("Task Manager App");
 });
-
 app.use("/api/v1/tasks", tasks);
 
 const port = 3000;
-app.listen(port, console.log("Server is listening on port 3000..."));
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log("Server is listening on port 3000..."));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
--- db/connect.js
+++ db/connect.js
@@ -1,2 +1,13 @@
-const connectionString =
-  "mongodb+srv://ashu:<password>@nodeexpressprojects.lulmkks.mongodb.net/?retryWrites=true&w=majority&appName=NodeExpressProjects";
+const mongoose = require("mongoose");
+
+const connectDB = (url) => {
+  return;
+  mongoose.connect(url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  });
+};
+
+module.exports = connectDB;
